1b41b41f22979ba7eb0b2b3b74affcf1b79d6257
Salman-Nur <salman.installer@gmail.com>
2024-09-08 12:53:08 +0600

    add a new project

---
+++ src/CP/Merge Two Sorted Lists/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+List<int> list1 = new List<int>{ 1, 1, 4, 5 };
+List<int> list2 = new List<int> { 2, 2, 3 };
+
+List<int> set = new List<int>();
+foreach (var item in list1)
+{
+    set.Add(item);
+}
+foreach (var item in list2)
+{
+    set.Add(item);
+}
+set.Sort();
+foreach (var item in set)
+{
+    Console.WriteLine(item);
+}
