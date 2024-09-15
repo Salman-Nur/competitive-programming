26c999a84e464b67acb22dcdcb5be7ff8d84d0d1
Salman-Nur <salman.installer@gmail.com>
2024-09-15 21:09:28 +0600

    Single Number

---
+++ src/CP/Single Number/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] numbers = { 1, 1, 3, 5, 5 };
+        int num = solution.SingleNumber(numbers);
+        Console.WriteLine(num);
+    }
+}
+
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int nonDuplicate = nums.GroupBy(x => x)
+            .Where(g => g.Count() == 1)
+            .Select(g => g.Key).First();
+
+        return nonDuplicate;
+    }
+}
