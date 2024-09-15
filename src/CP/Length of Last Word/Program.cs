8d44a34a0e7a310c2ec97054b929929bbb38358c
Salman-Nur <salman.installer@gmail.com>
2024-09-15 17:14:39 +0600

    Length of Last Word

---
+++ src/CP/Length of Last Word/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        string input = "Hello World";
+
+        int length = solution.LengthOfLastWord(input);
+        Console.WriteLine("Length of the last word: " + length);
+    }
+}
+
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        s = s.TrimEnd();
+
+        StringBuilder sb = new StringBuilder();
+
+        if (s.Length == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ')
+                {
+                    break;
+                }
+                else
+                {
+                    sb.Append(s[i]);
+                }
+            }
+            return sb.Length;
+        }
+    }
+}
