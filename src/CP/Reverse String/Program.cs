3f6594d5cb9a8f69e6e7c471081bdfada118aa7b
Salman-Nur <salman.installer@gmail.com>
2024-09-16 17:59:54 +0600

    Truncate Sentence

---
+++ src/CP/Reverse String/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution sln = new();
+        char[] s = { 'a', 'b', 'b', 'c', 'd' };
+        sln.ReverseString(s);
+    }
+}
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        Array.Reverse(s);
+    }
+}
---
+++ src/CP/Truncate Sentence/Program.cs
@@ -0,0 +1,30 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string str = "Hello how are you Contestant";
+        int k = 4;
+        str = solution.TruncateSentence(str, k);
+        Console.WriteLine(str);
+    }
+}
+
+public class Solution
+{
+    public string TruncateSentence(string s, int k)
+    {
+        //string result = string.Empty;
+        //string[] str = s.Split(' ');
+
+        //for (int i = 0; i < k; i++)
+        //{
+        //    result += str[i] + " ";
+        //}
+        //result = result.TrimEnd();
+        //return result;
+
+        return String.Join(" ", s.Split(" ").Take(k));
+    }
+}
